637d8c583507f9ff73887844fb406ec4a0e5e119
stefan-garofalo <stefan.garofalo@promemoriagroup.com>
2024-05-19 16:00:03 +0200

    feat: reference param infinite loop

--- components/helloworld.jsx
+++ components/helloworld.jsx
@@ -4,6 +4,6 @@ import { use } from 'react'
 import { getData } from '@/lib/api'
 
 export default function HelloWorld() {
-  const data = use(getData(1))
+  const data = use(getData([1]))
   return <div className="font-bold">{JSON.stringify(data)}</div>
 }
