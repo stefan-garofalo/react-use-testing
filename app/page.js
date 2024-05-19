f3ff3c1df3f162b7a9127718d8e6d58e9296bd5c
stefan-garofalo <stefan.garofalo@promemoriagroup.com>
2024-05-19 15:56:58 +0200

    fix: correct caching react.use

--- app/page.js
+++ app/page.js
@@ -6,22 +6,24 @@ import { menu, homepage, footer } from '@/lib/cms'
 import { Suspense } from 'react'
 
 export default async function Home() {
-  const menudata = menu()
-  const homepagedata = homepage()
-  const footerdata = footer()
+  // const menudata = menu()
+  // const homepagedata = homepage()
+  // const footerdata = footer()
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <Suspense fallback={<div>Loading header...</div>}> */}
-      <Header data={menudata} />
-      {/* </Suspense> */}
-      <Suspense fallback={<div>Loading page...</div>}>
+      {/* 
+        <Suspense fallback={<div>Loading header...</div>}> 
+          <Header data={menudata} /> 
+        </Suspense>
+      */}
+      {/* <Suspense fallback={<div>Loading page...</div>}>
         <Page data={homepagedata} />
-      </Suspense>
+      </Suspense> */}
       <HelloWorld />
-      <Suspense fallback={<div>Loading footer...</div>}>
+      {/* <Suspense fallback={<div>Loading footer...</div>}>
         <Footer data={footerdata} />
-      </Suspense>
+      </Suspense> */}
     </main>
   )
 }
