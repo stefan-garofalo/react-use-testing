307947f4f91cc995ea62e040cdff34c438cd9850
stefan-garofalo <stefan.garofalo@promemoriagroup.com>
2024-06-24 16:44:15 +0200

    feat: upgrade to next react rc

--- app/api/route.js
+++ app/api/route.js
@@ -1,4 +1,5 @@
-export async function GET(request) {
-  await new Promise((resolve) => setTimeout(resolve, 1500))
-  return Response.json({ message: 'Hello World' })
+export async function POST(request) {
+  const body = await request.json();
+  await new Promise(resolve => setTimeout(resolve, 1500));
+  return Response.json({ message: "Hello World" + JSON.stringify(body) });
 }
--- components/helloworld.jsx
+++ components/helloworld.jsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
-import { use } from 'react'
-import { getData } from '@/lib/api'
+import { use } from "react";
+import { getData } from "@/lib/api";
 
 export default function HelloWorld() {
-  const data = use(getData(1))
-  return <div className="font-bold">{JSON.stringify(data)}</div>
+  const data = use(getData([1]));
+  return <div className="font-bold">{JSON.stringify(data)}</div>;
 }
